769a4ae02971ec61222daeebbe263bd37ee30341
Mouzone <sunnyliu010@gmail.com>
2026-02-03 12:47:56 -0500

    Generate the table using js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 import multipliers from "./multipliers.json" with { type: "json" };
 
-const cells = document.querySelectorAll("td[data-row][data-col]");
-cells.forEach((cell) => {
-	const multiplier = multipliers[cell.dataset.row][cell.dataset.col];
-	cell.dataset.value = multiplier;
+const tbody = document.querySelector("tbody");
 
-	if (multiplier !== 1) {
-		cell.textContent = multiplier;
-	}
+const types = Object.keys(multipliers);
+
+types.forEach((attack) => {
+	const row = document.createElement("tr");
+
+	const th = document.createElement("th");
+	th.textContent = attack;
+	th.setAttribute("data-type", attack);
+	row.appendChild(th);
+
+	types.forEach((defense) => {
+		const td = document.createElement("td");
+		const multiplier = multipliers[attack][defense];
+
+		if (multiplier !== 1) {
+			td.textContent = multiplier;
+		}
+
+		td.setAttribute("data-value", multiplier);
+		td.setAttribute("data-row", attack);
+		td.setAttribute("data-col", defense);
+
+		row.appendChild(td);
+	});
+
+	tbody.appendChild(row);
 });
