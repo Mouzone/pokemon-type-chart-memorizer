7481c08a35b823da61ab0a2374ae9f0f8c57d6ab
Mouzone <sunnyliu010@gmail.com>
2026-02-03 11:34:08 -0500

    Add color to cells depending on values

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import multipliers from "./multipliers.json" with { type: "json" };
 const cells = document.querySelectorAll("td[data-row][data-col]");
 cells.forEach((cell) => {
 	const multiplier = multipliers[cell.dataset.row][cell.dataset.col];
+	cell.dataset.value = multiplier;
+
 	if (multiplier !== 1) {
 		cell.textContent = multiplier;
 	}
