838603d9005117e68e5e57356d700c372f37064e
Mouzone <sunnyliu010@gmail.com>
2026-02-03 11:28:09 -0500

    Modify to remove the clutter of 1's

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-import multipliers from "./multipliers.json";
+import multipliers from "./multipliers.json" with { type: "json" };
 
 const cells = document.querySelectorAll("td[data-row][data-col]");
 cells.forEach((cell) => {
-	cell.textContent = multipliers[cell.dataset.row][cell.dataset.col];
+	const multiplier = multipliers[cell.dataset.row][cell.dataset.col];
+	if (multiplier !== 1) {
+		cell.textContent = multiplier;
+	}
 });
